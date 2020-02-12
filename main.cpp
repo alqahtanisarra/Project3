3f18bd348d62743abead579b7f27d3355eb5e8e9
alqahtanisarra <sarra.alqahtani@gmail.com>
2020-02-12 17:07:12 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cassert>
+using namespace std;
+
+// MergeSort function declarations
+void mergeSort(vector<string>& a, vector<string>& tmp, int left, int right);
+void mergeSortedLists(vector<string>& a, vector<string>& tmp, int leftPos, int rightPos, int rightEnd);
+
+// BubbleSort function declaration
+void swap (string *a, string *b);
+void bubbleSort(string *a, int n);
+
+int main() {
+
+    /* open input and ouput files here*/
+
+    // get input: vector length which is the total number of entries in your data set
+    int length;
+    cin >> length;
+
+
+    vector<string> v(length); // vector to be sorted
+    vector<string> t(length); // temporary workspace
+
+    // define a string pointer dynamically allocate an array of strings
+    /* your code here */
+
+    // populate and print input
+    cout << "Unsorted:" << endl;
+    for (int i = 0; i < v.size(); i++) {
+        /*reading from file here*/
+        cout << v.at(i) << '\t';
+    }
+    cout << endl;
+
+    // copy the list of strings from vector to array
+    /* your code here */
+
+    clock_t start_mergeSort = clock();
+    // sort vector using mergeSort
+    mergeSort(v,t, 0, v.size() - 1);
+    clock_t end_mergeSort = clock();
+
+    // check output, make sure vector is sorted after mergeSort
+    for(int i = 1; i < v.size(); i++) {
+        assert(v.at(i-1) <= v.at(i));
+    }
+
+    // check output, make sure array is sorted after bubbleSort
+    /* your code here */
+
+    clock_t start_bubbleSort = clock();
+    // sort array using bubbleSort
+    /* your code here */
+    clock_t end_bubbleSort = clock();
+
+
+    // print sorted vector after mergeSort into "merge.txt"
+    cout << "Sorted:" << endl;
+    for (int i = 0; i < v.size(); i++) {
+        /* replace this */
+        cout << v.at(i) << '\t';
+    }
+    cout << endl;
+
+    // print sorted array after bubbleSort into file "bubble.txt"
+    /* your code here */
+
+    // print elapsed time for mergeSort and bubbleSort
+    double elapsed_mergeSort = double(end_mergeSort - start_mergeSort) / CLOCKS_PER_SEC;
+    double elapsed_bubbleSort = double(end_bubbleSort - start_bubbleSort) / CLOCKS_PER_SEC;
+
+    cout << elapsed_mergeSort << " " << elapsed_bubbleSort << endl;
+
+    return 0;
+}
+
+void mergeSortedLists(vector<string>& a, vector<string>& tmp, int leftPos, int rightPos, int rightEnd) {
+    int leftEnd = rightPos - 1;
+    int tempPos = leftPos;
+    int numElements = rightEnd - leftPos + 1;
+
+    while (leftPos <= leftEnd && rightPos <= rightEnd) {
+        if (a[leftPos] <= a[rightPos]) {
+            tmp[tempPos++] = a[leftPos++];
+        } else {
+            tmp[tempPos++] = a[rightPos++];
+        }
+
+    }
+
+    while (leftPos <= leftEnd) {
+        tmp[tempPos++] = a[leftPos++];
+    }
+    while (rightPos <= rightEnd) {
+        tmp[tempPos++] = a[rightPos++];
+    }
+
+    for (int i = 0; i < numElements; i++, --rightEnd) {
+        a[rightEnd] = tmp[rightEnd];
+    }
+}
+
+void mergeSort(vector<string>& a, vector<string>& tmp, int left, int right) {
+    if (left < right) {
+        int center = ( left + right ) / 2;
+        mergeSort(a, tmp, left, center);
+        mergeSort(a, tmp, center + 1, right);
+        mergeSortedLists(a, tmp, left, center + 1, right);
+    }
+}
+
+// Swap function
+void swap(string *a, string *b) {
+    /* your code here */
+}
+
+// BubbleSort function
+void bubbleSort(string *a, string n) {
+    /* your code here */
+}
